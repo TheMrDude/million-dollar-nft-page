ab20b96918e435a8c42713181cee03f45ff7a5e4
TheMrDude <mrdudesblog@gmail.com>
2025-02-06 13:46:47 -0500

    Initial commit: Million Dollar NFT Page

--- github_push.bat
+++ github_push.bat
@@ -8,16 +8,20 @@ REM Set Git configuration
 REM Initialize repository
 "C:\Program Files\Git\cmd\git.exe" init
 
+REM Remove existing remote if it exists
+"C:\Program Files\Git\cmd\git.exe" remote remove origin 2>nul
+
+REM Add remote repository
+"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/TheMrDude/million-dollar-nft-page.git
+
 REM Add all files
 "C:\Program Files\Git\cmd\git.exe" add .
 
 REM Commit changes
 "C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit: Million Dollar NFT Page"
 
-REM Add remote repository
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/TheMrDude/million-dollar-nft-page.git
-
-REM Push to GitHub
-"C:\Program Files\Git\cmd\git.exe" push -u origin main
+REM Rename master to main and force push
+"C:\Program Files\Git\cmd\git.exe" branch -M master main
+"C:\Program Files\Git\cmd\git.exe" push -u -f origin main
 
 echo Repository push complete!
